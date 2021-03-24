869c093f5b1abfd6a0023650b0eb77301bfd3518
kyungryun <kyung_ryun@naver.com>
2021-03-24 17:53:44 +0900

    간단 dag 생성

---
+++ dags/etl_test.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+import random
+import pymysql
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2021, 3, 20)
+}
+
+dag = DAG('etl_dag',
+          default_args=default_args,
+          schedule_interval='*/2 * * * *',
+          catchup=False
+          )
+conn = pymysql.connect(host='localhost', user='airflow', password='airflow', db='airflow', charset='utf8')
+
+
+def select_data():
+    cursor = conn.cursor()
+
+    select_sql = "select max(random_number) from airflow_test"
+    cursor.execute(select_sql)
+
+    now = datetime.now()
+    nowDatetime = now.strftime('%Y-%m-%d %H:%M')
+    f = open("dir" + str(nowDatetime) + ".txt", 'w')
+    for data in cursor.fetchall():
+        f.write(str(data[0]))
+    f.close()
+    conn.commit()
+    conn.close()
+    return 'CreateFile'
+
+
+def insert_data():
+    cursor = conn.cursor()
+
+    insert_sql = "insert into airflow_test values (%s, now())"
+    cursor.execute(insert_sql, random.randint(1, 100))
+    conn.commit()
+    conn.close()
+    return 'Insert Data'
+
+
+insert_task = PythonOperator(
+    task_id='insert_task',
+    python_callable=insert_data,
+    dag=dag
+)
+select_task = PythonOperator(
+    task_id='select_task',
+    python_callable=select_data,
+    dag=dag
+)
+
+insert_task >> select_task
